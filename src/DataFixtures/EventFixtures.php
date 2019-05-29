8bf44f93ca0e85a69fe8f2749f1c013dd4a37246
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-05-29 15:13:57 +0200

    création fixtures event

---
+++ src/DataFixtures/EventFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Event;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class EventFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker  =  Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 15; $i++) {
+            $event = new Event();
+            $event->setTitle($faker->realText($maxNbChars = 100, $indexSize = 2));
+            $event->setDescription($faker->realText($maxNbChars = 300, $indexSize = 2));
+            $event->setPicture($faker->imageUrl(500, 600, 'news'));
+            $manager->persist($event);
+        }
+        $manager->flush();
+    }
+}
