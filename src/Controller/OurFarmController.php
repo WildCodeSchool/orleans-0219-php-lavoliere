1f0a99a3b98b15d343aa19e6c96792ecafb76cf0
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-06-05 13:50:36 +0200

    Creation of files to show Our Farm page

---
+++ src/Controller/OurFarmController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OurFarmController extends AbstractController
+{
+    /**
+     * @Route("/our/farm", name="our_farm")
+     */
+    public function index()
+    {
+        return $this->render('our_farm/index.html.twig');
+    }
+}
