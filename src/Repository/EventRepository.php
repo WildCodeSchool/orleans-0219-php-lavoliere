0b06434bfe3b568b0b8d19f5ccc4c222a00aa0ac
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-06-05 11:00:56 +0200

    deletion commented code in repositories

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -34,33 +34,4 @@ class EventRepository extends ServiceEntityRepository
             ;
         return $query->getResult();
     }
-
-    // /**
-    //  * @return Event[] Returns an array of Event objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('e')
-            ->andWhere('e.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('e.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Event
-    {
-        return $this->createQueryBuilder('e')
-            ->andWhere('e.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
