936169800cf1593aba26ec1f0af709162aa70d61
maiwenn99 <maiwenngay99@gmail.com>
2019-06-06 17:12:38 +0200

    jednf

---
+++ src/Controller/CatalogController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class CatalogController extends AbstractController
+{
+    /**
+     * @Route("/catalog", name="catalog")
+     */
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->render('catalog/index.html.twig', [
+            'products' => $productRepository->findAll(),
+        ]);
+    }
+}
