1190b1d4d74242ad163bd7d3629f1fc01bc0889a
maiwenn99 <maiwenngay99@gmail.com>
2019-06-07 09:45:18 +0200

    create design and findAll for page product

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
