796469762d50f572282e456500b2b07a66905409
maiwenn99 <maiwenngay99@gmail.com>
2019-06-07 10:16:44 +0200

    create scss file for catalog page

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class CatalogController extends AbstractController
 {
     /**
