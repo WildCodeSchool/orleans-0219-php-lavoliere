f85b116360e1fc54febf164c772ff755cba65596
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-06-07 13:30:26 +0200

    Fix graphism cards

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -22,12 +22,12 @@ class ProductFixtures extends Fixture implements DependentFixtureInterface
         for ($i=0; $i <= 13; $i++) {
             $product = new Product();
             $product->setName($faker->word);
-            $product->setBundle($faker->sentence(4, true));
+            $product->setBundle($faker->word);
             $product->setPrice($faker->randomFloat(2, 0, 30));
             $product->setDescription($faker->sentence(4, true));
             $product->setOrigin($faker->word);
             $product->setPicture($faker->imageUrl(480, 640, 'food'));
-            $product->setIsShowcased($faker->boolean(5));
+            $product->setIsShowcased($faker->boolean(10));
             $product->setIsAvailable($faker->boolean(90));
             $product->setCategory($this->getReference('category_' . rand(0, 3)));
             $manager->persist($product);
