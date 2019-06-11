ae71932f7dbe79999086e0595ce4c1d9a2a563f2
Vinny <paulvinny@gmail.com>
2019-06-11 15:22:09 +0200

    change productController path and routes

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use App\Form\ProductType;
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/product")
+ * @Route("/admin/product")
  */
 class ProductController extends AbstractController
 {
