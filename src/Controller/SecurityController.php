04e6b11f5c6e7911858bef5f7305f3747f2b95ae
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-06-12 15:24:03 +0200

    Made the log-in and registretion in two different pages

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -15,24 +12,14 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user, [
-            'action' => $this->generateUrl('app_register'),
-        ]);
-        $form->handleRequest($request);
-
-
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'registrationForm' => $form->createView(),
-            'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,10 +19,24 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('lastname', TextType::class, [
+                'required' => true,
+                'label' => 'Nom',
+            ])->add('firstname', TextType::class, [
+                'required' => true,
+                'label' => 'Prénom',
+            ])
             ->add('email', EmailType::class, [
                 'required' => true,
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('phone', TelType::class, [
+                'required' => true,
+                'label' => 'Téléphone',
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [ 'label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Entrez à nouveau le mot de passe'],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
