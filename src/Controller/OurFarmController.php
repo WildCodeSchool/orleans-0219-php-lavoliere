d8baaa50ef64d541e88767189a9a6a525df2fe16
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-06-12 09:58:10 +0200

    Stash on new branch

--- src/Controller/OurFarmController.php
+++ src/Controller/OurFarmController.php
@@ -14,6 +14,7 @@ class OurFarmController extends AbstractController
      */
     public function index(LocationRepository $locationRepository)
     {
+
         return $this->render('our_farm/index.html.twig', [
             'locations' => $locationRepository->findAll(),
         ]);
