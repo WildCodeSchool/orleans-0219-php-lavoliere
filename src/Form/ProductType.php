013865ea31c841318af37246948c441efbfbe92e
Vinny <paulvinny@gmail.com>
2019-06-12 19:39:13 +0200

    stylize new products page

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,16 +15,55 @@ class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
         $builder
-            ->add('name')
-            ->add('bundle')
-            ->add('price')
-            ->add('description')
-            ->add('origin')
-            ->add('picture')
-            ->add('isShowcased')
-            ->add('isAvailable')
-        ;
+            ->add('name', TextType::class, [
+                'label' => 'Nom :',
+                'attr' => ['class' => 'col-8'],
+                'invalid_message' => 'Nom de produit obligatoire',
+            ])
+
+            ->add('bundle', TextType::class, [
+                'label' => 'Lot :',
+                'attr' => ['class' => 'col-8'],
+                'invalid_message' => 'Lot obligatoire',
+            ])
+
+            ->add('price', TextType::class, [
+                'label' => 'Prix :',
+                'attr' => ['class' => 'col-8'],
+                'invalid_message' => 'Lot obligatoire',
+            ])
+
+            ->add('origin', TextType::class, [
+                'label' => 'Origine :',
+                'required' => false,
+                'attr' => ['class' => 'col-8'],
+                'invalid_message' => 'Lot obligatoire',
+            ])
+
+            ->add('description', TextareaType::class, [
+                'label' => 'Description :',
+                'attr' => ['class' => 'col-8', 'rows' => '4', 'cols' => '80'],
+                'invalid_message' => 'Lot obligatoire',
+            ])
+
+            ->add('picture', FileType::class, [
+                'label' => 'Image :',
+                'attr' => ['class' => 'col-md-4'],
+                'invalid_message' => 'Veuillez importer une image',
+            ])
+
+            ->add('isShowcased', CheckboxType::class, [
+                'label' => 'Top du moment',
+                'required' => false,
+            ])
+
+            ->add('isAvailable', CheckBoxType::class, [
+                'label' => 'En ligne',
+                'required' => false,
+                'attr' => ['checked' => 'checked']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
