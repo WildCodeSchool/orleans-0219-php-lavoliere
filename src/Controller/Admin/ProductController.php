394fe96e33c570391424486867fd6daed525d498
Vinny <paulvinny@gmail.com>
2019-06-12 13:41:01 +0200

    stylize admin product index

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -21,7 +21,7 @@ class ProductController extends AbstractController
     public function index(ProductRepository $productRepository): Response
     {
         return $this->render('product/index.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $productRepository->findAllSortByName(),
         ]);
     }
 
