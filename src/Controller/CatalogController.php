b39983bae5888fe2c1651cd8e69f0db02b5735aa
maiwenn99 <maiwenngay99@gmail.com>
2019-06-12 10:29:33 +0200

    add header week basket

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,12 +11,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CatalogController extends AbstractController
 {
+    const BASKET_CATEGORY = 'Panier de la semaine';
+
     /**
      * @Route("/catalog", name="catalog")
      */
     public function index(ProductRepository $productRepository): Response
     {
+        $weekBasketName = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => self::BASKET_CATEGORY]);
+
+        $weekBasket = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->findOneBy(['category' => $weekBasketName->getId()]);
+
         return $this->render('catalog/index.html.twig', [
+            'weekBasket' => $weekBasket,
             'products' => $productRepository->findAll(),
         ]);
     }
