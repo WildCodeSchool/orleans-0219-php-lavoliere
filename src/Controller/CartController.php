010bae2a3c7b428e880ca0987b04af7593cb94d7
Solennelle <43750777+solennelle@users.noreply.github.com>
2019-06-14 11:25:44 +0200

    Fix retrait code test

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -24,12 +24,6 @@ class CartController extends AbstractController
 
         $user = $this->getUser();
 
-        /* code pour tester la boucle provenant de session */
-        $cart[] = ['quantity' => 2, 'product' => $productRepository->find(250)];
-        $cart[] = ['quantity' => 1, 'product' => $productRepository->find(251)];
-        $cart[] = ['quantity' => 3, 'product' => $productRepository->find(252)];
-        /* à retirer lors de l'ajout de la fonctionnalité d'ajout au panier*/
-
         $session->set('cart', $cart);
         $cart = $session->get('cart');
         return $this->render('cart/index.html.twig', [
