809816206fe1447b2a72c7f9bda64a78bc2d0d6e
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-06-14 11:01:34 +0200

    fix scss

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -24,9 +24,9 @@ class CartController extends AbstractController
         $user = $this->getUser();
 
         /* code pour tester la boucle provenant de session */
-        $cart[] = ['quantity' => 2, 'product'=> $productRepository->find(250)];
-        $cart[] = ['quantity' => 1, 'product'=> $productRepository->find(251)];
-        $cart[] = ['quantity' => 3, 'product'=> $productRepository->find(252)];
+        $cart[] = ['quantity' => 2, 'product' => $productRepository->find(250)];
+        $cart[] = ['quantity' => 1, 'product' => $productRepository->find(251)];
+        $cart[] = ['quantity' => 3, 'product' => $productRepository->find(252)];
         /* à retirer lors de l'ajout de la fonctionnalité d'ajout au panier*/
 
         $session->set('cart', $cart);
@@ -34,7 +34,6 @@ class CartController extends AbstractController
         return $this->render('cart/index.html.twig', [
             'user' => $user,
             'cart' => $cart,
-            ]);
+        ]);
     }
-
 }
