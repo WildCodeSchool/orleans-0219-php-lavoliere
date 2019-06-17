fe29971abbe56cb63e2eab91446287d554de23cf
Vinny <paulvinny@gmail.com>
2019-06-17 18:07:19 +0200

    add Location form

---
+++ src/Form/LocationType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('adress')
+            ->add('postal_code')
+            ->add('city')
+            ->add('delivery_date')
+            ->add('location_type')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
