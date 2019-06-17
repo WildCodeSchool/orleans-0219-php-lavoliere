c47a800221c8a6f1e1e8ca4c8c382a0957a68f04
Vinny <paulvinny@gmail.com>
2019-06-17 19:16:21 +0200

    "stylize location order"
    This reverts commit fe29971abbe56cb63e2eab91446287d554de23cf.

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -15,26 +15,20 @@ class OrderController extends AbstractController
      * @param SessionInterface $session
      * @param ProductRepository $productRepository
      */
-    public function delivery(SessionInterface $session, ProductRepository $productRepository, Location $location)
+    public function delivery(SessionInterface $session, ProductRepository $productRepository)
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         if (!$session->has('cart')) {
             $session->set('cart', []);
         }
         $user = $this->getUser();
-//        $cart = [];
-        /* code pour tester la boucle provenant de session */
-        $cart[] = ['quantity' => 2, 'product' => $productRepository->find(1)];
-        $cart[] = ['quantity' => 1, 'product' => $productRepository->find(2)];
-        $cart[] = ['quantity' => 3, 'product' => $productRepository->find(3)];
-        /* à retirer lors de l'ajout de la fonctionnalité d'ajout au panier*/
+        $cart = [];
 
         $session->set('cart', $cart);
         $cart = $session->get('cart');
         return $this->render('order/delivery.html.twig', [
             'user' => $user,
             'cart' => $cart,
-            'location' => $location,
         ]);
     }
 }
