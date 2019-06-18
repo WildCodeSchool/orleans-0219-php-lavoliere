756b73b286e5a449ae564235d737ae447a51417f
Vinny <paulvinny@gmail.com>
2019-06-18 13:47:38 +0200

    change Delivery method on OrdreController

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Location;
+use App\Repository\LocationRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -14,9 +14,15 @@ class OrderController extends AbstractController
      * @Route("/livraison", name="delivery")
      * @param SessionInterface $session
      * @param ProductRepository $productRepository
+     * @param LocationRepository $location
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function delivery(SessionInterface $session, ProductRepository $productRepository)
-    {
+    public function delivery(
+        SessionInterface $session,
+        ProductRepository $productRepository,
+        LocationRepository $location
+    ) {
+
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         if (!$session->has('cart')) {
             $session->set('cart', []);
@@ -29,6 +35,7 @@ class OrderController extends AbstractController
         return $this->render('order/delivery.html.twig', [
             'user' => $user,
             'cart' => $cart,
+            'locations' => $location->findAll(),
         ]);
     }
 }
