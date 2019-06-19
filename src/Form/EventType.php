96dc35d6e994bd35a5b58982bd5fc6b089eb1750
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-06-19 18:32:55 +0200

    add view of the event form

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -13,17 +16,34 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
+            ->add('title', TextType::class, [
+                'required' => true,
+                'label' => 'Titre',
+                'label_attr' => ['class' => 'col-md-12'],
+
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'label' => 'Description',
+                'label_attr' => ['class' => 'col-md-12'],
+            ])
             ->add('pictureFile', VichImageType::class, [
                 'required' => false,
                 'label' => 'Image',
+                'label_attr' => ['class' => 'col-md-12'],
                 'allow_delete' => false,
                 'download_link' => false,
             ])
-            ->add('picture')
-            ->add('startAt')
-            ->add('endAt');
+            ->add('startAt', DateType::class, [
+                'required' => false,
+                'label' => 'Début',
+                'label_attr' => ['class' => 'col-md-12'],
+            ])
+            ->add('endAt', DateType::class, [
+                'required' => false,
+                'label' => 'Fin',
+                'label_attr' => ['class' => 'col-md-12'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
