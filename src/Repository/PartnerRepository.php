aa0745df1436b0538178418912e8fa398eb4b524
maiwenn99 <47444313+maiwenn99@users.noreply.github.com>
2019-07-01 17:13:08 +0200

    Update PartnerRepository.php

--- src/Repository/PartnerRepository.php
+++ src/Repository/PartnerRepository.php
@@ -18,33 +18,4 @@ class PartnerRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Partner::class);
     }
-
-    // /**
-    //  * @return Partener[] Returns an array of Partener objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Partener
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
