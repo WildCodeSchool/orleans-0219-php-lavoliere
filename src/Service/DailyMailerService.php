700ad82ae60f0df73775db42170353d1d250d9a2
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-07-01 14:34:06 +0200

    Complete DailyMailerService

--- src/Service/DailyMailerService.php
+++ src/Service/DailyMailerService.php
@@ -2,16 +2,37 @@
 
 namespace App\Service;
 
+use App\Repository\PurchaseRepository;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Templating\EngineInterface;
+
 class DailyMailerService
 {
-    public $mailer;
+    private $mailer;
+    private $params;
+    private $templating;
 
-    public function __construct(MailerService $mailer)
+    public function __construct(MailerService $mailer, ParameterBagInterface $params, EngineInterface $templating)
     {
         $this->mailer = $mailer;
+        $this->params = $params;
+        $this->templating = $templating;
     }
 
-    public function sendDailyMail()
+    public function sendDailyMail(PurchaseRepository $purchaseRepository)
     {
+        $orders = $purchaseRepository->findActualDayOrders();
+
+        $sender = $this->params->get('mailer_from');
+        $destination = $this->params->get('mailer_from');
+        $body = $this->templating->render('emails/dailyMail.html.twig', ['orders' => $orders]);
+
+        $this->mailer->sendMail(
+            $sender,
+            $destination,
+            'Commandes à préparer pour aujourdh\'ui !',
+            'text/html',
+            $body
+        );
     }
 }
