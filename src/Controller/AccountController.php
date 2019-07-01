ecb9dfd4cd37fbfeb47380375cb133d0c758589a
Vinny <paulvinny@gmail.com>
2019-07-01 09:04:18 +0200

    stylize purchase history

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,11 +6,13 @@ use App\Entity\Purchase;
 use App\Form\ChangePasswordType;
 use App\Repository\PurchaseRepository;
 use App\Service\OrderService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
 use App\Form\UserInformationType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -95,20 +97,21 @@ class AccountController extends AbstractController
     }
 
     /**
-     * @
+     * @IsGranted("ROLE_USER")
      * @Route("/historique-commande", name="account_history")
      */
-    public function seeHistory(OrderService $orderService)
-    {
+    public function seeHistory(
+        OrderService $orderService,
+        PurchaseRepository $purchaseRepository
+    ) {
         $user = $this->getUser();
-        $purchases = $this->getDoctrine()
-            ->getRepository(Purchase::class)
-            ->findPurchasesByDescOrderDate($user);
+
+        $purchases = $purchaseRepository->findPurchasesByDescOrderDate($user);
 
         return $this->render('account/history.html.twig', [
             'user' => $user,
             'purchases' => $purchases,
             'total' => $orderService,
-            ]);
+        ]);
     }
 }
