2eaf5ed19c182fcc6505ae3bc2dd66304ba8e71c
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-07-02 12:10:03 +0200

    Remove comment code

--- src/Repository/PartnerRepository.php
+++ src/Repository/PartnerRepository.php
@@ -18,33 +18,4 @@ class PartnerRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Partner::class);
     }
-
-    // /**
-    //  * @return Partner[] Returns an array of Partner objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Partner
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
