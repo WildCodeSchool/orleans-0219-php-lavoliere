dcdd402077b21542f7249fa39f31d091987e6495
maiwenn99 <maiwenngay99@gmail.com>
2019-07-02 10:16:56 +0200

    change style of index for admin partners

--- src/DataFixtures/PartnersFixtures.php
+++ src/DataFixtures/PartnersFixtures.php
@@ -22,12 +22,10 @@ class PartnersFixtures extends Fixture
         for ($i = 0; $i < 10; $i++) {
             $event = new Partners();
             $event->setName($faker->realText($maxNbChars = 100, $indexSize = 2));
-            $event->setDescription($faker->realText($maxNbChars = 300, $indexSize = 2));
+            $event->setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
             $event->setPicture($faker->imageUrl(500, 600, 'food'));
             $manager->persist($event);
         }
         $manager->flush();
     }
-
 }
-
