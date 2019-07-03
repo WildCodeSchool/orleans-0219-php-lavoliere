1aac6246cd84523994f2994ae1e47f9ddbf29690
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-07-03 16:06:58 +0200

    remove console.log

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,24 +1,13 @@
 const $ = require('jquery');
 
 $(document).ready(function () {
-    $(".collapse-contact").on('shown.bs.collapse', function () {
-        window.location = "#collapseContact";
-    });
-    $(".contact-toggle").click(function () {
-        $(".collapse-contact").collapse('show');
-    });
-});
-
-$(document).ready(function () {
 
     // Javascript to enable link to tab
     let hash = window.location.hash ;
     let clean_hash = hash.substring(1);
     if (hash) {
         let id_product = document.getElementById(clean_hash);
-        console.log(id_product);
         let tab_pill = (id_product.closest('.tab-pane')).id;
-        console.log(tab_pill);
         $('#'+tab_pill).tab('show');
         document.getElementById(clean_hash).scrollIntoView();
     }
