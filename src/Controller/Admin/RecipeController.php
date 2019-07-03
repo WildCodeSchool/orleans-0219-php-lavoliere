8a708ce142a59478805cb508b5ae971d8dca9cb9
maiwenn99 <maiwenngay99@gmail.com>
2019-07-03 11:14:15 +0200

    create form and view for recipe admin

--- src/Controller/Admin/RecipeController.php
+++ src/Controller/Admin/RecipeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Recipe;
+use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,6 +22,29 @@ class RecipeController extends AbstractController
     {
         return $this->render('recipe/index.html.twig', [
             'recipes' => $recipeRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/nouveau", name="recipe_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recipe_index');
+        }
+
+        return $this->render('recipe/new.html.twig', [
+            'product' => $recipe,
+            'form' => $form->createView(),
         ]);
     }
 }
