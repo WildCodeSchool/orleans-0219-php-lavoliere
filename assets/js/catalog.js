470761fb92530c78254ea1af1175aeebb69c258b
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-07-07 19:37:32 +0200

    fix js

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,13 +1,14 @@
 const $ = require('jquery');
 
 $(document).ready(function () {
-
     let hash = window.location.hash ;
     let clean_hash = hash.substring(1);
     if (hash) {
         let id_product = document.getElementById(clean_hash);
         let tab_pill = (id_product.closest('.tab-pane')).id;
-        $('#'+tab_pill).tab('show');
+        let hash_tab = '#'+tab_pill;
+        let activeTab = $('[href="' + hash_tab + '"]');
+        activeTab && activeTab.tab('show');
         document.getElementById(clean_hash).scrollIntoView();
     }
 });
