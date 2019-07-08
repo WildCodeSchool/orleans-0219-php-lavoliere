8c94f3430eae5119755d704870fde44c967bd8ca
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-07-08 09:44:42 +0200

    js fonctional

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,30 +1,16 @@
 const $ = require('jquery');
 
-// $(document).ready(function () {
-//     let hash = window.location.hash ;
-//     let clean_hash = hash.substring(1);
-//     let id_product = document.getElementById(clean_hash);
-//     let tab_pill = (id_product.closest('.tab-pane')).id;
-//     let hash_tab = '#'+tab_pill;
-//     let activeTab = $('[href="' + hash_tab + '"]');
-//     if (hash) {
-//         activeTab && activeTab.tab('show');
-//         $(activeTab).on('shown', function () {
-//             document.getElementById(clean_hash).scrollIntoView();
-//         });
-//     }
-// });
-
 $(document).ready(function () {
-
     let hash = window.location.hash ;
     let clean_hash = hash.substring(1);
     if (hash) {
         let id_product = document.getElementById(clean_hash);
         let tab_pill = (id_product.closest('.tab-pane')).id;
-        $('#'+tab_pill).tab('show');
-        document.getElementById(clean_hash).scrollIntoView();
+        let hash_tab = '#'+tab_pill;
+        let activeTab = $('[href="' + hash_tab + '"]');
+        hash_tab && activeTab.tab('show');
+        activeTab.on('shown.bs.tab', function () {
+            document.getElementById(clean_hash).scrollIntoView();
+        });
     }
 });
-
-
