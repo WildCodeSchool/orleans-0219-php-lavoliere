d421eca46ba652eea3014478b1be041e1ff133bd
Vinny <paulvinny@gmail.com>
2019-07-08 13:52:51 +0200

    cart JS refactored

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -4,17 +4,7 @@ function onClickDecrement(event) {
         .then(function (response) {
             return response.json();
         })
-        .then(function (response) {
-            const id_product = response.id;
-            const spanQuantity = document.getElementById("quantity-selector-" + id_product);
-            const spanTotal = document.getElementById("product-total-" + id_product);
-            const ResultCart = document.getElementById("result-cart");
-            const cartCount = document.getElementById("cart-count");
-            spanQuantity.textContent = response.quantity;
-            spanTotal.textContent = response.totalProduct.toFixed(2) + ' €';
-            ResultCart.textContent = response.totalCart.toFixed(2) + ' € TTC';
-            cartCount.textContent = response.cartCount;
-        });
+        .then(updateCartData);
 }
 
 document.querySelectorAll('a.cart-decrement').forEach(function (link) {
@@ -28,18 +18,20 @@ function onClickIncrement(event) {
         .then(function (response) {
             return response.json();
         })
-        .then(function (response) {
-            const id_product = response.id;
-            const spanQuantity = document.getElementById("quantity-selector-" + id_product);
-            const spanTotal = document.getElementById("product-total-" + id_product);
-            const ResultCart = document.getElementById("result-cart");
-            const cartCount = document.getElementById("cart-count");
-            spanQuantity.textContent = response.quantity;
-            spanTotal.textContent = response.totalProduct.toFixed(2) + ' €';
-            ResultCart.textContent = response.totalCart.toFixed(2) + ' € TTC';
-            cartCount.textContent = response.cartCount;
-        });
+        .then(updateCartData);
 
+}
+
+function updateCartData(response) {
+    const id_product = response.id;
+    const spanQuantity = document.getElementById("quantity-selector-" + id_product);
+    const spanTotal = document.getElementById("product-total-" + id_product);
+    const ResultCart = document.getElementById("result-cart");
+    const cartCount = document.getElementById("cart-count");
+    spanQuantity.textContent = response.quantity;
+    spanTotal.textContent = response.totalProduct.toFixed(2) + ' €';
+    ResultCart.textContent = response.totalCart.toFixed(2) + ' € TTC';
+    cartCount.textContent = response.cartCount;
 }
 
 document.querySelectorAll('a.cart-increment').forEach(function (link) {
